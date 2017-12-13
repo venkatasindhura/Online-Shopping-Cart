02664dce5085b19df616cb3766ec2d1145cb3d52
sindhura <sindhura.g1990@gmail.com>
2017-12-13 18:36:52 +1100

    Successfully loaded static resources

---
+++ onlineshoppingcartUI/src/main/java/com/ksn/onlineshoppingcart/controller/PageController.java
@@ -0,0 +1,40 @@
+package com.ksn.onlineshoppingcart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+// +ctrl shift o to import all annaotations at a time in eclipse
+
+@Controller
+public class PageController {
+	@RequestMapping(value = {"/" ,"/home" ,"/index"})
+	public ModelAndView index(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","Welcome to spring web mvc");
+		return mv;
+		
+	}
+  /*@RequestMapping(value="/test")
+  public ModelAndView test(@RequestParam(value="greeting",required = false)String greeting){
+	  if(greeting == null){
+		  greeting = "Hello there";
+	  }
+	  ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	  
+  }
+  
+  @RequestMapping(value="/test/{greeting}")
+  public ModelAndView testpathvariable(@PathVariable(value="greeting")String greeting){
+	  if(greeting == null){
+		  greeting = "Hello there";
+	  }
+	  ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	  
+  }*/
+}
